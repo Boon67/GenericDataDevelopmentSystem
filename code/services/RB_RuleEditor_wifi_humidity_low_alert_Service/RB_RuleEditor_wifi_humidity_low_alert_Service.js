36a3478fe8af885549bcf225ce54e6e5f40140be
Terrance Boon <Boon67@msn.com>
2018-09-26 09:53:03 -0500

    Asset created by Boon67

---
+++ code/services/RB_RuleEditor_wifi_humidity_low_alert_Service/RB_RuleEditor_wifi_humidity_low_alert_Service.js
@@ -0,0 +1,58 @@
+function RB_RuleEditor_wifi_humidity_low_alert_Service (req, resp) {
+    
+    var reqObject;
+
+    try {
+        reqObject = JSON.parse(req.params);
+    } catch(e) {
+        reqObject = req.params;
+    }
+  
+    if ((tryParse(reqObject.body).Humidity < "5")) {
+	  callAlertProvider0();
+  }
+  
+    
+    
+    function callAlertProvider0 () {
+      
+        ClearBlade.init({request:req});
+        ClearBlade.Code().execute("alert_service", Object.assign({ruleName: 'wifi_humidity_low_alert'}, req.params), true, function (err, body){
+            if(err) {
+                log("Failure while executing alert_service; " + JSON.stringify(err));
+                callAlertProvider1();
+            } else {
+                log("Successfully executed alert_service");
+                callAlertProvider1();
+            }
+        })
+    
+    }
+    
+    
+    function callAlertProvider1 () {
+      
+        ClearBlade.init({request:req});
+        ClearBlade.Code().execute("alert_log", Object.assign({ruleName: 'wifi_humidity_low_alert'}, req.params), true, function (err, body){
+            if(err) {
+                log("Failure while executing alert_log; " + JSON.stringify(err));
+                resp.error(body);
+            } else {
+                log("Successfully executed alert_log");
+                resp.success(body);
+            }
+        })
+    
+    }
+    
+    
+    resp.success('Nothing to do');
+  }
+ 
+  function tryParse(str) {
+    try {
+      return JSON.parse(str);
+    } catch (e) {
+      return str;
+    }
+}
