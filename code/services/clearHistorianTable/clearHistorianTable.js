df7888ea69ad366dbee2b6fe5c2e6af1a62049e1
Terrance Boon <Boon67@msn.com>
2018-09-26 09:53:15 -0500

    Asset created by Boon67

---
+++ code/services/clearHistorianTable/clearHistorianTable.js
@@ -0,0 +1,17 @@
+TABLENAME="historian"
+function clearHistorianTable(req, resp) {
+    ClearBlade.init({request:req});
+    var query = ClearBlade.Query();
+    query.setPage(0, 0);
+    query.notEqualTo('item_id', '00000000-0000-0000-0000-000000000000');
+    var callback = function (err, data) {
+        if (err) {
+        	resp.error("update error : " + JSON.stringify(data));
+        } else {
+        	  resp.success(data);
+        }
+    };
+
+   	var col = ClearBlade.Collection({collectionName: TABLENAME});
+    col.remove(query, callback);
+}
